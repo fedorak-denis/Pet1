fa3bc2f6754b8a1608f8acecf0f4833d7b322705
fedorak-denis <thefedorak@gmail.com>
2022-08-21 00:43:54 +0300

    Update 001_create_tables.sql

--- migrations/001_create_tables.sql
+++ migrations/001_create_tables.sql
@@ -1,6 +1,6 @@
 CREATE TABLE User (
-    id int NOT NULL AUTO_INCREMENT,
-    login varchar(255) NOT NULL,
-    email varchar(255) NOT NULL,
-    password varchar(255) NOT NULL
+    id INT NOT NULL AUTO_INCREMENT,
+    login VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
 );
